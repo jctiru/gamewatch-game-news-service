572c6eb30d022299ff27da135c3b4efb3697428e
jctiru <jonathanchristiantiru@gmail.com>
2020-06-27 18:58:30 +0800

    Initial commit

---
+++ gamewatch-game-news-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamenewsservice/GamewatchGameNewsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jctiru.gamewatch.api.gamewatchgamenewsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamewatchGameNewsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamewatchGameNewsServiceApplication.class, args);
+	}
+
+}
---
+++ gamewatch-game-news-service/src/test/java/com/jctiru/gamewatch/api/gamewatchgamenewsservice/GamewatchGameNewsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jctiru.gamewatch.api.gamewatchgamenewsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamewatchGameNewsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
