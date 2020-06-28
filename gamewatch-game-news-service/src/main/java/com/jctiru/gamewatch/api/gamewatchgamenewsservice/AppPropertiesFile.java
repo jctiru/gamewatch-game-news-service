5aea409981e2a5db9a665d4768ea83d509341309
jctiru <jonathanchristiantiru@gmail.com>
2020-06-28 23:16:18 +0800

    Create AppPropertiesFile utility class

---
+++ gamewatch-game-news-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamenewsservice/AppPropertiesFile.java
@@ -0,0 +1,21 @@
+package com.jctiru.gamewatch.api.gamewatchgamenewsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppPropertiesFile {
+
+	private Environment env;
+
+	@Autowired
+	public AppPropertiesFile(Environment env) {
+		this.env = env;
+	}
+
+	public String getIgdbApiKey() {
+		return env.getProperty("igdb.api.key");
+	}
+
+}
