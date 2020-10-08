99cbedd2a1de0648e74ae6e65a6d17acb67d73bb
jctiru <jonathanchristiantiru@gmail.com>
2020-10-08 20:39:23 +1000

    Add game spot article image

---
+++ gamewatch-game-news-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamenewsservice/io/external/model/response/GameSpotArticleImage.java
@@ -0,0 +1,42 @@
+package com.jctiru.gamewatch.api.gamewatchgamenewsservice.io.external.model.response;
+
+public class GameSpotArticleImage {
+
+	private String square_tiny;
+	private String screen_tiny;
+	private String square_small;
+	private String original;
+
+	public String getSquare_tiny() {
+		return square_tiny;
+	}
+
+	public void setSquare_tiny(String square_tiny) {
+		this.square_tiny = square_tiny;
+	}
+
+	public String getScreen_tiny() {
+		return screen_tiny;
+	}
+
+	public void setScreen_tiny(String screen_tiny) {
+		this.screen_tiny = screen_tiny;
+	}
+
+	public String getSquare_small() {
+		return square_small;
+	}
+
+	public void setSquare_small(String square_small) {
+		this.square_small = square_small;
+	}
+
+	public String getOriginal() {
+		return original;
+	}
+
+	public void setOriginal(String original) {
+		this.original = original;
+	}
+
+}
